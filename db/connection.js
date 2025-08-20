4661f066430bf47a28d384738997d6ab347293f4
marzia <you@example.com>
2025-08-20 17:00:48 +0200

    add connection

---
+++ db/connection.js
@@ -0,0 +1,24 @@
+// import mysql2
+
+const mysql2 = require("mysql2");
+
+// database connection
+
+const credentials = {
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+};
+
+const connection = mysql2.createConnection(credentials);
+
+connection.connect((err) => {
+  if (err) {
+    throw err;
+  }
+  console.info("Connection successo");
+  console.log(connection);
+});
+
+module.exports = connection;
