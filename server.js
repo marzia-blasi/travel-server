fe3ae06a31240f277868e233d6cf7acf7a77b0ea
marzia <you@example.com>
2025-08-20 13:50:29 +0200

    initial server

---
+++ server.js
@@ -0,0 +1,24 @@
+// 1 IMPORT EXPRESS
+
+const express = require("express");
+const app = express();
+const PORT = process.env.PORT;
+
+// 2 import CORS
+const cors = require("cors");
+app.use(express.json());
+app.use(cors());
+
+//3 static asset
+app.use(express.static("img"));
+
+// 4 router ( da inserire)
+
+app.get("/", (req, res) => {
+  console.log("ciao");
+  res.send("Hello World!");
+});
+
+app.listen(PORT, () => {
+  console.log(`Example app listening on port ${PORT}`);
+});
